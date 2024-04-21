4515996883d3afdebff9c9cb504a593fe6979ac7
Asma-malica <115386995+Asma-malica@users.noreply.github.com>
2024-04-21 19:18:17 +0530

    initial

---
+++ JS_Operators/script.js
@@ -0,0 +1,18 @@
+// Arithmetic Operators
+let a = 5
+let b = 6
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a**b) //power
+console.log(a/b)
+console.log(a%b)
+console.log(++a) // pre increment
+console.log(a)
+console.log(a++) // post increment
+console.log(a)
+console.log(--a) // pre decrement
+console.log(a)
+console.log(a--) // post decrement
+console.log(a)
+
